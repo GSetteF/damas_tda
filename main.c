2d39742dc0db6955465c9de42ca6252e85c91bbf
Gabriel Sette de Figueirêdo <gabrielsettef@gmail.com>
2023-11-26 03:53:10 -0300

    Pequenas correções

--- main.c
+++ main.c
@@ -28,6 +28,14 @@ typedef struct Pecas{
         
 }Pecas;
 
+typedef struct Diagonal{
+        
+        char tipo;
+        int i;
+        int j;
+        
+}Diagonal;
+
 
 void exibir_matriz(Pecas matriz[8][8]) {
         int i = 0, j = 0;
@@ -44,12 +52,7 @@ void exibir_matriz(Pecas matriz[8][8]) {
         }
         printf("\n");
     }
-}
-
-
-void iniciar_partida(){
-        puts("Comandos para iniciar a partida, etc");
-}        
+}    
 
 
 int verificar_jogada (int jogador, int i_origem, int j_origem, int i_destino, int j_destino, Pecas tabuleiro[8][8], Pecas vazio){
@@ -141,50 +144,90 @@ int verificar_jogada (int jogador, int i_origem, int j_origem, int i_destino, in
                         puts("Movimento inválido! você tentou mover a peça para uma posição que não existe. \n");
                         return(0);
                 }
-                else if (distancia_i >= 2){ //capturando uma peca
-                
-                        int i_peca_capturada = (i_origem+i_destino)/2;
-                        int j_peca_capturada = (j_origem+j_destino)/2;
-                        
-                        if (j_origem < j_destino){
-                                        j_peca_capturada = (j_destino - 1);
-                                if (i_origem < i_destino){
-                                        i_peca_capturada = (i_destino - 1);
-                                                                }else{
-                                                                        i_peca_capturada = (i_destino + 1);
-                                                                }
-                                
-                        }else if (j_origem > j_destino){
-                                        j_peca_capturada = (j_destino + 1);
-                                if (i_origem > i_destino){
-                                         i_peca_capturada = (i_destino + 1);
-                                                                }else{
-                                                                        i_peca_capturada = (i_destino - 1);
-                                                                }
-                               
-                        }
-                        if (tabuleiro[i_peca_capturada][j_peca_capturada].jogador != 0 && tabuleiro[i_peca_capturada][j_peca_capturada].jogador != jogador){
-                                tabuleiro[i_peca_capturada][j_peca_capturada] = vazio;
-                        }
-                        tabuleiro[i_destino][j_destino] = tabuleiro[i_origem][j_origem];
-                        tabuleiro[i_origem][j_origem] = vazio;
-                        if (jogador == 1){
-                                placar_player_1 += 1;
-                                printf("\nO player 1 capturou %d peça(s)\n\n", placar_player_1);
-                                if (i_destino == 0){
-                                        tabuleiro[i_destino][j_destino].tipo = 'D';
-                                }
-                        }else if (jogador == 2){
-                                placar_player_2 += 1;
-                                printf("\nO player 2 capturou %d peça(s)\n\n", placar_player_2);
-                                if (i_destino == 7){
-                                        tabuleiro[i_destino][j_destino].tipo = 'D';
-                                }
-                        
-                        return(1);
-                }else{
-                        printf("\nMovimento inválido. Você não pode caturar uma peça.\n\n");
-                        }
+                else if (distancia_i == 1){ //adando uma casa	
+                	tabuleiro[i_destino][j_destino] = tabuleiro[i_origem][j_origem];
+                	tabuleiro[i_origem][j_origem] = vazio;
+                    return(1); 
+                }
+                else{
+                	Diagonal diagonal[10]; //diagonal que a peca deseja percorrer
+                	int indice = 0;
+                	int i_diag = i_origem, j_diag = j_origem;
+                	char dif_i = '-', dif_j = '-';
+                	
+                	//determinando o sentido e posicao inicial da diagonal
+                	if (i_destino > i_origem){
+                		dif_i = '+';
+						i_diag+=1;}
+                	else { 
+						i_diag -=1;}
+					if (j_destino > j_origem){
+						dif_j = '+';
+                		j_diag+=1;}
+                	else { 
+						j_diag -=1;}
+					
+					//adicionando itens no vetor diagonal
+					while(i_destino != i_diag){
+						//printf("casa sendo acessada: [%d][%d]  conteudo: %c \n", i_diag, j_diag, tabuleiro[i_diag][j_diag].tipo);
+						
+						//caso a peca seja do mesmo jogador
+						if(tabuleiro[i_diag][j_diag].jogador == jogador){
+							diagonal[indice].tipo = 'A';
+							diagonal[indice].i = i_diag;
+							diagonal[indice].j = j_diag;
+						}
+						//caso a peca seja do outro jogador
+						else if(tabuleiro[i_diag][j_diag].tipo == 'P' || tabuleiro[i_diag][j_diag].tipo == 'D'){
+							diagonal[indice].tipo = 'P';
+							diagonal[indice].i = i_diag;
+							diagonal[indice].j = j_diag;
+						} else{ //caso o espaco seja vazio
+							diagonal[indice].tipo = 'V';
+							diagonal[indice].i = i_diag;
+							diagonal[indice].j = j_diag;
+						}
+						
+					
+						indice+=1;
+					
+						if(dif_i == '+'){
+							i_diag += 1;
+						} else{ i_diag -= 1;}
+						if(dif_j == '+'){
+							j_diag += 1;
+						} else{ j_diag -= 1;}	
+					}
+					//criando um limite da diagonal
+					diagonal[indice].tipo = 'F';
+					diagonal[indice].i = i_diag;
+					diagonal[indice].j = j_diag;
+					
+					/* for(indice = 0; indice < 10; indice++){
+						printf("Conteudo : %c, i: %d, j: %d \n", diagonal[indice].tipo, diagonal[indice].i, diagonal[indice].j);
+					} */
+					
+					indice = 0;
+					while(diagonal[indice].tipo != 'F'){
+						if(diagonal[indice].tipo == 'P' && diagonal[indice+1].tipo == 'P' || diagonal[indice].tipo == 'A'|| diagonal[indice+1].tipo == 'A'){
+							puts("Movimento inválido!");
+							return(0);
+						}
+						else if(diagonal[indice].tipo == 'P'){
+							tabuleiro[diagonal[indice+1].i][diagonal[indice+1].j] = tabuleiro[i_origem][j_origem];
+                			tabuleiro[i_origem][j_origem] = vazio;
+                			tabuleiro[diagonal[indice].i][diagonal[indice].j] = vazio;
+                    		return(1); 
+						}
+						indice+=1;
+					}
+					//mover a peca
+					tabuleiro[i_destino][j_destino] = tabuleiro[i_origem][j_origem];
+                	tabuleiro[i_origem][j_origem] = vazio;
+                	
+                	return(1);
+                	
+                	
                 }
         }
 }
@@ -271,17 +314,28 @@ void jogar_partida(){
         //populando o tabuleiro
         preencher_tabuleiro(tabuleiro, P1, P2, vazio);
         
-        
-        
         char condicao_de_parada;
         int jogador_atual = 1;
         int i_origem = 0, j_origem = 0, i_destino = 0, j_destino = 0;
         int ver_jogada = 0;
+        int ver_fim = 0;
         
-        //Depois, melhor mudar essa verificacao por uma funcao que verifica
-        while (condicao_de_parada != 'V' || condicao_de_parada != 'E'){
+        while (ver_fim == 0){
                 puts("Tabuleiro atual: \n");
                 exibir_matriz(tabuleiro);
+                ver_fim = verificar_fim(tabuleiro, P1, P2, vazio);
+                if (verificar_fim == 1){
+                	puts("Jogador 1 Venceu!");
+                	break;
+                }
+                else if (verificar_fim == 2){
+                	puts("Jogador 2 Venceu!");
+                	break;
+                }
+                else if (verificar_fim == 3){
+                	puts("O jogo terminou em empate :(");
+                	break;
+                }
                 puts("\nEscreva a posição da peça que deseja mover e então a posição para que deseja movê-la separados tudo por espaço.");
                 scanf("%d %d %d %d", &i_origem, &j_origem, &i_destino, &j_destino);
                 ver_jogada = verificar_jogada(jogador_atual, i_origem, j_origem, i_destino, j_destino, tabuleiro, vazio);
@@ -346,12 +400,43 @@ void exibir_creditos(){
         puts("    \__//\n\n");        
 }
 
+int verificar_fim(Pecas tabuleiro[8][8] , Pecas P1, Pecas P2, Pecas vazio){
+	int tipo_fim = 0;
+	int i = 0, j = 0;
+	int cont_1 = 0, cont_2 = 0;
+	
+	
+	//contar as pecas restantes
+	for(i = 0; i < 8;i++){
+		for(j = 0; j < 8;j++){
+			if (tabuleiro[i][j].jogador == 1){
+				cont_1+=1;
+			}
+			else if (tabuleiro[i][j].jogador == 2){
+				cont_2+=1;
+			}
+		}
+	}
+	//jogador 2 ganhou
+	if (cont_1 == 0){
+		return(2);
+	}
+	//jogador 1 ganhou
+	else if (cont_2 == 0){
+		return(1);
+	}
+	//empate(1 peca de cada um restante)
+	else if (cont_1 == 1 && cont_2 == 1){
+		return(3);
+	}
+	return(0);
+}
 
 int main(int argc, char *argv[]) {
     //Comando para mudar a lingua
     setlocale(LC_ALL, "Portuguese");
         
-    int op_menu = 0; //variavel responsavel pela escolha das opcoes no menu principal
+    	int op_menu = 0; //variavel responsavel pela escolha das opcoes no menu principal
         char op_loop; //variavel principal pela repeticao do codigo
         
         do{
